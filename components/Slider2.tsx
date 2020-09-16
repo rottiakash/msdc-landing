6c2de6fa977d00a8258ce74d37adeea27e9c6641
Akash Rotti <rottiakash@gmail.com>
2020-09-16 19:05:04 +0530

    Section 2 Animation

--- components/Slider2.tsx
+++ components/Slider2.tsx
@@ -1,16 +1,29 @@
 export interface Slider2Props {}
-
+import React, { useState } from "react";
+import { InView } from "react-intersection-observer";
+import styles from "./Slider2.module.css";
 const Slider2: React.FunctionComponent<Slider2Props> = () => {
+  const [visible, set] = useState(false);
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
         height: "100vh",
       }}
     >
-      TO BE ADDED
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <InView onChange={(inView, entry) => set(inView)} threshold={1}>
+          <div className={visible ? styles.introtext : styles.hide}>
+            Your Safety is Important to us
+          </div>
+        </InView>
+      </div>
     </div>
   );
 };
